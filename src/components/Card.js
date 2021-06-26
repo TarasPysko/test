8482cbc7c739f735f9ad17f1bc270a9d8c2d1bd7
TarasPysko <pusko111@ukr.net>
2021-06-26 10:57:55 +0300

    last change

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ function Card({ user }) {
 
   return (
     <div className="card">
-      <img className="card-image-top" src={user.avatar_url} alt={user.login} />
+      <img className="card-image-top" src={user.avatar_url} alt={user.login} style={{maxWidth="150px"}} />
       <div className="card-body">
         <h5 className="card-title">{user.login}</h5>
         <Link to={"/profile/" + user.login} className="btn btn-primary">
