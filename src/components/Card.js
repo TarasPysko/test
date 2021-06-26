5190df08d209b33ae1ae016d40bf6e1c5ec24a80
TarasPysko <pusko111@ukr.net>
2021-06-26 10:59:58 +0300

    last version

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Card({ user }) {
-
   return (
     <div className="card">
-      <img className="card-image-top" src={user.avatar_url} alt={user.login} style={{maxWidth="150px"}} />
+      <img
+        className="card-image-top"
+        src={user.avatar_url}
+        alt={user.login}
+        style={{ maxHeight="150px"}}
+      />
       <div className="card-body">
         <h5 className="card-title">{user.login}</h5>
         <Link to={"/profile/" + user.login} className="btn btn-primary">
